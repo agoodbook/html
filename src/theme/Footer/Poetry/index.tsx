1232cbfb1335ea008a42f5fe2e644f970f4a4612
agoodbook <beher0@foxmail.com>
2023-12-02 00:46:40 +0800

    fix: 修复底部空白

--- src/theme/Footer/Poetry/index.tsx
+++ src/theme/Footer/Poetry/index.tsx
@@ -20,7 +20,7 @@ export default function Poetry() {
           JSON.stringify({ ...data, updateAt: Date.now() }),
         );
       })
-      .catch(async (err) => {
+      .catch(async () => {
         await messageApi.error("无法正确获取诗句");
       });
   }
@@ -51,27 +51,34 @@ export default function Poetry() {
   }, []);
 
   return (
-    <div className="custom-footer">
-      {contextHolder}
-      <Tooltip color="#2db7f5" placement="top" title="点击复制">
-        <span
-          onMouseEnter={() => setIsShowName(true)}
-          onMouseLeave={() => setIsShowName(false)}
-          className="hitokoto"
-          onClick={handleCopyHitokoto}
-        >
-          「 {info.poetry}」
-        </span>
-      </Tooltip>
+    <>
+      {info.poetry ? (
+        <div className="custom-footer">
+          {contextHolder}
+          <Tooltip color="#2db7f5" placement="top" title="点击复制">
+            <span
+              onMouseEnter={() => setIsShowName(true)}
+              onMouseLeave={() => setIsShowName(false)}
+              className="hitokoto"
+              onClick={handleCopyHitokoto}
+            >
+              「 {info.poetry}」
+            </span>
+          </Tooltip>
 
-      <span
-        style={{
-          opacity: isShowName ? 1 : 0,
-        }}
-        className="from"
-      >
-        {info.name || "来源网络"}
-      </span>
-    </div>
+          <div
+            style={{
+              opacity: isShowName ? 1 : 0,
+              paddingTop: "60px",
+            }}
+            className="from"
+          >
+            {info.name || "来源网络"}
+          </div>
+        </div>
+      ) : (
+        ""
+      )}
+    </>
   );
 }
