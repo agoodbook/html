c4e137f4b775af4d16ee18a85a7cf95aa701cbe8
agoodbook <beher0@foxmail.com>
2023-12-02 18:07:39 +0800

    update: 更新algolia搜索配置

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,14 +1,14 @@
-import {themes as prismThemes} from 'prism-react-renderer';
-import type {Config} from '@docusaurus/types';
-import type * as Preset from '@docusaurus/preset-classic';
+import { themes as prismThemes } from "prism-react-renderer";
+import type { Config } from "@docusaurus/types";
+import type * as Preset from "@docusaurus/preset-classic";
 
 const username: string = "agoodbook";
 const repo: string = "html";
 
 const config: Config = {
-  title: 'AGoodBook', // 站点名称
-  tagline: '书山有路勤为径，学海无涯苦作舟',  // slogan，标语
-    favicon: 'img/favicon.ico',
+  title: "AGoodBook", // 站点名称
+  tagline: "书山有路勤为径，学海无涯苦作舟", // slogan，标语
+  favicon: "img/favicon.ico",
 
   // 配置网站站点url
   url: `https://agoodbook.github.io/`,
@@ -19,62 +19,61 @@ const config: Config = {
   // GitHub部署配置
   organizationName: username, // 用户名
   projectName: repo, // 仓库名
-  deploymentBranch: 'gh-pages',
+  deploymentBranch: "gh-pages",
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
 
   i18n: {
-    defaultLocale: 'zh-CN',
-    locales: ['en', 'zh-CN'],
+    defaultLocale: "zh-CN",
+    locales: ["en", "zh-CN"],
     localeConfigs: {
       en: {
-        label: 'English',
-        direction: 'ltr',
-        htmlLang: 'en',
+        label: "English",
+        direction: "ltr",
+        htmlLang: "en",
       },
-      'zh-CN': {
-        label: '中文',
-        direction: 'ltr',
-        htmlLang: 'zh-CN',
-      }
-    }
+      "zh-CN": {
+        label: "中文",
+        direction: "ltr",
+        htmlLang: "zh-CN",
+      },
+    },
   },
   presets: [
     [
-      'classic',
+      "classic",
       {
         docs: {
-          sidebarPath: './sidebars.ts',
+          sidebarPath: "./sidebars.ts",
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            `https://github.com/${username}/${repo}/tree/main`,
-          lastVersion: 'current',
+          editUrl: `https://github.com/${username}/${repo}/tree/main`,
+          lastVersion: "current",
           versions: {
             current: {
-              label: 'HTML5',
-              path: 'next',
+              label: "HTML5",
+              path: "next",
             },
-            '4.0.0': {
-              label: 'HTML4',
-              path: '4.0.0',
-              banner: 'none',
+            "4.0.0": {
+              label: "HTML4",
+              path: "4.0.0",
+              banner: "none",
             },
-            '3.0.0': {
-              label: 'HTML3',
-              path: '3.0.0',
-              banner: 'none',
+            "3.0.0": {
+              label: "HTML3",
+              path: "3.0.0",
+              banner: "none",
             },
-            '2.0.0': {
-              label: 'HTML2',
-              path: '2.0.0',
-              banner: 'none',
-            }
+            "2.0.0": {
+              label: "HTML2",
+              path: "2.0.0",
+              banner: "none",
+            },
           },
         },
         theme: {
-          customCss: './src/css/custom.css',
+          customCss: "./src/css/custom.css",
         },
       } satisfies Preset.Options,
     ],
@@ -87,67 +86,68 @@ const config: Config = {
       },
     },
     versions: {
-      '2.0.0': 'versioned_docs/version-2.0.0',
-      '3.0.0': 'versioned_docs/version-3.0.0',
-      '4.0.0': 'versioned_docs/version-4.0.0',
-      '5.0.0': 'docs'
+      "2.0.0": "versioned_docs/version-2.0.0",
+      "3.0.0": "versioned_docs/version-3.0.0",
+      "4.0.0": "versioned_docs/version-4.0.0",
+      "5.0.0": "docs",
       // 添加更多版本...
     },
-    themes: ['@docusaurus/theme-search-algolia'],
+    themes: ["@docusaurus/theme-search-algolia"],
     algolia: {
-      appId: 'X1Z85QJPUV',
-      apiKey: 'bf7211c161e8205da2f933a02534105a',
-      indexName: 'docusaurus-2',
+      appId: "WHMBQSPQOS",
+      apiKey: "9f430fb0112fb4bb367bfc56009d9770",
+      indexName: "html",
+      contextualSearch: true,
     },
     // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: "img/docusaurus-social-card.jpg",
     navbar: {
-      title: 'AGoodBook', // 导航上站点名称
+      title: "AGoodBook", // 导航上站点名称
       hideOnScroll: true,
       logo: {
-        alt: 'AGoodBook', //  站点 logo 文字替换
-        src: 'img/logo.svg',  //   站点 logo  链接
+        alt: "AGoodBook", //  站点 logo 文字替换
+        src: "img/logo.svg", //   站点 logo  链接
       },
       items: [
         {
-          to: 'docs/next',            // 点击后跳转的链接，站内跳转用 to ,站外用 href
-          activeBasePath: 'docs',     // 根据它显示当前高亮
-          label: '文档',               // 显示的名称
-          position: 'left',           // 显示在导航的 左边 还是 右边
+          to: "docs/next", // 点击后跳转的链接，站内跳转用 to ,站外用 href
+          activeBasePath: "docs", // 根据它显示当前高亮
+          label: "文档", // 显示的名称
+          position: "left", // 显示在导航的 左边 还是 右边
         },
         //   right
         {
-          type: 'search',
-          position: 'left',
-          className: 'custom-search'
+          type: "search",
+          position: "left",
+          className: "custom-search",
         },
         {
-          type: 'docsVersionDropdown',
-          position: 'right',
+          type: "docsVersionDropdown",
+          position: "right",
         },
         {
-          type: 'localeDropdown',
-          position: 'right',
+          type: "localeDropdown",
+          position: "right",
           dropdownItemsAfter: [
             {
-              type: 'html',
+              type: "html",
               value: '<hr style="margin: 0.3rem 0;">',
             },
             {
               href: `https://github.com/${username}/${repo}/issues/3526`,
-              label: '帮助我们翻译',
+              label: "帮助我们翻译",
             },
           ],
         },
         {
           href: `https://github.com/${username}/${repo}`,
-          label: 'GitHub',
-          position: 'right',
+          label: "GitHub",
+          position: "right",
         },
       ],
     },
     footer: {
-      style: 'light',
+      style: "light",
       copyright: `Copyright © ${new Date().getFullYear()} AGoodBook.`,
     },
     prism: {
